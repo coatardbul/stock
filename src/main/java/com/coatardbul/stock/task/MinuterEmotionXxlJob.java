0cf246bad84c4be595e146aaad7722090fe6b057
xiaolei <xiaolei@shubidatech.com>
2022-02-17 15:15:56 +0800

    股票

--- src/main/java/com/coatardbul/stock/task/MinuterEmotionXxlJob.java
+++ src/main/java/com/coatardbul/stock/task/MinuterEmotionXxlJob.java
@@ -1,7 +1,13 @@
 package com.coatardbul.stock.task;
 
+import com.coatardbul.stock.common.util.JsonUtil;
+import com.coatardbul.stock.model.dto.StockEmotionDayDTO;
+import com.coatardbul.stock.service.statistic.StockMinuteEmotinStaticService;
+import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
@@ -17,14 +23,17 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 @Component
 public class MinuterEmotionXxlJob {
+    @Autowired
+    StockMinuteEmotinStaticService stockMinuteEmotinStaticService;
 
     @XxlJob("minuterEmotionJobHandler")
     public void minuterEmotionJobHandler() throws Exception {
-        //todo 还是需要传递参数的
-        log.info("分钟情绪定时任务开始");
-        //ssss
-
-
+        String param = XxlJobHelper.getJobParam();
+        log.info("分钟情绪定时任务开始,传递参数为：" + param);
+        if (StringUtils.isNotBlank(param)) {
+            StockEmotionDayDTO stockEmotionDayDTO = JsonUtil.readToValue(param, StockEmotionDayDTO.class);
+            stockMinuteEmotinStaticService.taskRefreshDay(stockEmotionDayDTO);
+        }
         log.info("分钟情绪定时任务结束");
 
     }
