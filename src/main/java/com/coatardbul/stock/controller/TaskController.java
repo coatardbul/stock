97336b73b82a6ed9ecf3742051570fb6d337e993
xiaolei <xiaolei@shubidatech.com>
2022-02-20 15:27:32 +0800

    股票

---
+++ src/main/java/com/coatardbul/stock/controller/TaskController.java
@@ -0,0 +1,57 @@
+package com.coatardbul.stock.controller;
+
+import com.coatardbul.stock.common.annotation.WebLog;
+import com.coatardbul.stock.common.api.CommonResult;
+import com.coatardbul.stock.common.util.DateTimeUtil;
+import com.coatardbul.stock.common.util.JsonUtil;
+import com.coatardbul.stock.model.dto.StockEmotionDayDTO;
+import com.coatardbul.stock.model.dto.StockStaticTemplateBaseDTO;
+import com.coatardbul.stock.service.statistic.StockMinuteEmotinStaticService;
+import com.coatardbul.stock.task.DayStatisticJob;
+import com.coatardbul.stock.task.MinuterEmotionXxlJob;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
+import java.util.Date;
+
+/**
+ * <p>
+ * Note:
+ * <p>
+ * Date: 2022/2/18
+ *
+ * @author Su Xiaolei
+ */
+@Slf4j
+@RestController
+@Api(tags = "")
+@RequestMapping("/task")
+public class TaskController {
+
+    @Autowired
+    DayStatisticJob dayStatisticJob;
+    @Autowired
+    StockMinuteEmotinStaticService stockMinuteEmotinStaticService;
+
+    @WebLog(value = "")
+    @RequestMapping(path = "/dayStatic", method = RequestMethod.POST)
+    public CommonResult dayStatic() {
+        dayStatisticJob.dayStaticJobHandler();
+        return null;
+    }
+
+
+    @WebLog(value = "")
+    @RequestMapping(path = "/minuterEmotion", method = RequestMethod.POST)
+    public CommonResult minuterEmotion(@Validated @RequestBody  StockEmotionDayDTO stockEmotionDayDTO) throws Exception {
+        stockMinuteEmotinStaticService.taskRefreshDay(stockEmotionDayDTO);
+        return  null;
+    }
+}
